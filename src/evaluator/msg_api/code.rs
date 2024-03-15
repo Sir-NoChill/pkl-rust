3ccb33b824a5d16113e1117bffa5548ae012b375
Ayrton Chilibeck <aa@chilibeck.com>
2024-03-15 09:05:38 -0600

    Added Optionals for ommitted fields

--- src/evaluator/msg_api/code.rs
+++ src/evaluator/msg_api/code.rs
@@ -3,17 +3,17 @@
 /// more details
 
 
-const CODE_NEW_EVALUATOR                 :u8 = 0x20;
-const CODE_NEW_EVALUATOR_RESPONSE        :u8 = 0x21;
-const CODE_CLOSE_EVALUATOR               :u8 = 0x22;
-const CODE_EVALUATE                      :u8 = 0x23;
-const CODE_EVALUATE_RESPONSE             :u8 = 0x24;
-const CODE_EVALUATE_LOG                  :u8 = 0x25;
-const CODE_EVALUATE_READ                 :u8 = 0x26;
-const CODE_EVALUATE_READ_RESPONSE        :u8 = 0x27;
-const CODE_EVALUATE_READ_MODULE          :u8 = 0x28;
-const CODE_EVALUATE_READ_MODULE_RESPONSE :u8 = 0x29;
-const CODE_LIST_RESOURCES_REQUEST        :u8 = 0x2a;
-const CODE_LIST_RESOURCES_RESPONSE       :u8 = 0x2b;
-const CODE_LIST_MODULES_REQUEST          :u8 = 0x2c;
-const CODE_LIST_MODULES_RESPONSE         :u8 = 0x2d;
+pub const CODE_NEW_EVALUATOR                 :u8 = 0x20;
+pub const CODE_NEW_EVALUATOR_RESPONSE        :u8 = 0x21;
+pub const CODE_CLOSE_EVALUATOR               :u8 = 0x22;
+pub const CODE_EVALUATE                      :u8 = 0x23;
+pub const CODE_EVALUATE_RESPONSE             :u8 = 0x24;
+pub const CODE_EVALUATE_LOG                  :u8 = 0x25;
+pub const CODE_EVALUATE_READ                 :u8 = 0x26;
+pub const CODE_EVALUATE_READ_RESPONSE        :u8 = 0x27;
+pub const CODE_EVALUATE_READ_MODULE          :u8 = 0x28;
+pub const CODE_EVALUATE_READ_MODULE_RESPONSE :u8 = 0x29;
+pub const CODE_LIST_RESOURCES_REQUEST        :u8 = 0x2a;
+pub const CODE_LIST_RESOURCES_RESPONSE       :u8 = 0x2b;
+pub const CODE_LIST_MODULES_REQUEST          :u8 = 0x2c;
+pub const CODE_LIST_MODULES_RESPONSE         :u8 = 0x2d;
--- src/evaluator/msg_api/outgoing.rs
+++ src/evaluator/msg_api/outgoing.rs
@@ -1,4 +1,4 @@
-#![allow(nonstandard_style)]
+#![allow(nonstandard_style)] // for compatibililty with pkl messages
 use std::collections::HashMap;
 
 use rmp_serde as rmps;
@@ -45,10 +45,10 @@ pub struct Checksums {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ProjectOrDependency {
-    packageUri: String,
+    packageUri: Option<String>,
     r#type: String,
-    projectFileUri: String,
-    checksums: Checksums,
+    projectFileUri: Option<String>,
+    checksums: Option<Checksums>,
     dependencies: HashMap<String, ProjectOrDependency>,
 }
 impl OutgoingMessage for ProjectOrDependency {}
@@ -56,27 +56,34 @@ impl OutgoingMessage for ProjectOrDependency {}
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CreateEvaluator {
     requestId: i64,
-    clientResourceReaders: Vec<ResourceReader>,
-    clientModuleReaders: Vec<ModuleReader>,
-    modulePaths: Vec<String>,
-    env: HashMap<String, String>,
-    properties: HashMap<String, String>,
-    outputFormat: String,
-    allowedModules: Vec<String>,
-    allowedResources: Vec<String>,
-    rootDir: String,
-    cacheDir: String,
-    project: ProjectOrDependency,
+    clientResourceReaders: Option<Vec<ResourceReader>>,
+    clientModuleReaders: Option<Vec<ModuleReader>>,
+    modulePaths: Option<Vec<String>>,
+    env: Option<HashMap<String, String>>,
+    properties: Option<HashMap<String, String>>,
+    outputFormat: Option<String>,
+    allowedModules: Option<Vec<String>>,
+    allowedResources: Option<Vec<String>>,
+    rootDir: Option<String>,
+    cacheDir: Option<String>,
+    project: Option<ProjectOrDependency>,
+    timeoutSeconds: Option<i64>,
 }
 impl OutgoingMessage for CreateEvaluator {}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CloseEvaluator {
+    evaluatorId: Option<i64>,
+}
+impl OutgoingMessage for CloseEvaluator {}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Evaluate {
     requestId: i64,
     evaluatorId: i64,
     moduleUri: String,
-    moduleText: String,
-    expr: String,
+    moduleText: Option<String>,
+    expr: Option<String>,
 }
 impl OutgoingMessage for Evaluate {}
 
@@ -84,8 +91,8 @@ impl OutgoingMessage for Evaluate {}
 pub struct ReadResourceResponse {
     requestId: i64,
     evaluatorId: i64,
-    contents: Vec<u8>,
-    error: String,
+    contents: Option<Vec<u8>>,
+    error: Option<String>,
 }
 impl OutgoingMessage for ReadResourceResponse {}
 
@@ -93,8 +100,8 @@ impl OutgoingMessage for ReadResourceResponse {}
 pub struct ReadModuleResponse {
     requestId: i64,
     evaluatorId: i64,
-    contents: String,
-    error: String,
+    contents: Option<String>,
+    error: Option<String>,
 }
 impl OutgoingMessage for ReadModuleResponse {}
 
@@ -109,8 +116,8 @@ impl OutgoingMessage for PathElement {}
 pub struct ListResourceResponse {
     requestId: i64,
     evaluatorId: i64,
-    pathElements: Vec<PathElement>,
-    error: String,
+    pathElements: Option<Vec<PathElement>>,
+    error: Option<String>,
 }
 impl OutgoingMessage for ListResourceResponse {}
 
@@ -118,8 +125,8 @@ impl OutgoingMessage for ListResourceResponse {}
 pub struct ListModulesResponse {
     requestId: i64,
     evaluatorId: i64,
-    pathElements: Vec<PathElement>,
-    error: String,
+    pathElements: Option<Vec<PathElement>>,
+    error: Option<String>,
 }
 impl OutgoingMessage for ListModulesResponse {}
 
@@ -139,5 +146,32 @@ mod tests {
                             0x6F, 0x62, 0x62, 0x61, 0x62, 0x6C, 0x65, 0xC3, 0xA7, 0x69, 0x73, 0x4C, 0x6F, 0x63, 0x61, 0x6C, 0xC2];
 
         assert_eq!(msp, expected);
+    }
+
+    #[test]
+    /// Test against the specification docs
+    /// [
+    ///  0x2d,
+    ///  {
+    ///    "requestId": -6478924,
+    ///    "evaluatorId": -13901,
+    ///    "pathElements": [
+    ///      {
+    ///        "name": "foo.pkl",
+    ///        "isDirectory": false
+    ///      }
+    ///    ]
+    ///   }
+    /// ]
+    fn test_pack_message_specification_1() {
+        let pe = PathElement{name: "foo.pkl".into(), isDirectory: false};
+        let mr = ListModulesResponse{requestId: -647892, evaluatorId: -13901, pathElements: vec![pe], error: "".into()};
+
+        let mp = pack_message(&mr, code::CODE_LIST_MODULES_RESPONSE).unwrap();
+
+        let expected = vec![0x92, 0x2D, 0x83, 0xA9, 0x72, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x49, 0x64, 0xD2, 0xFF, 0x9D, 0x23, 0xB4, 0xAB, 0x65, 0x76, 0x61, 0x6C, 0x75, 0x61, 0x74, 0x6F, 0x72, 0x49, 0x64, 0xD1, 0xC9, 0xB3, 0xAC, 0x70, 0x61, 0x74, 0x68, 0x45, 0x6C, 0x65, 0x6D, 0x65, 0x6E, 0x74, 0x73, 0x91, 0x82, 0xA4, 0x6E, 0x61, 0x6D, 0x65, 0xA7, 0x66, 0x6F, 0x6F, 0x2E, 0x70, 0x6B, 0x6C, 0xAB, 0x69, 0x73, 0x44, 0x69, 0x72, 0x65, 0x63, 0x74, 0x6F, 0x72, 0x79, 0xC2];
+        println!("Serialized: {:X?}", mp);
+
+        assert_eq!(mp, expected);
     }
 }
