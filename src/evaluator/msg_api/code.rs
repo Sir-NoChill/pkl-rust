7d9336c9f2429afac9c596b3d42d901672f921f6
Ayrton Chilibeck <aa@chilibeck.com>
2024-03-15 00:51:47 -0600

    Experimented with the msgpack package
    Need to know:
    - Do we need to encode field names? (Assuming yes right now)
    - How do we encode field names? (the rmp_serde package is giving me
    trouble)

--- src/evaluator/msg_api/code.rs
+++ src/evaluator/msg_api/code.rs
@@ -3,17 +3,17 @@
 /// more details
 
 
-static const CODE_NEW_EVALUATOR                 u8 = 0x20;
-static const CODE_NEW_EVALUATOR_RESPONSE        u8 = 0x21;
-static const CODE_CLOSE_EVALUATOR               u8 = 0x22;
-static const CODE_EVALUATE                      u8 = 0x23;
-static const CODE_EVALUATE_RESPONSE             u8 = 0x24;
-static const CODE_EVALUATE_LOG                  u8 = 0x25;
-static const CODE_EVALUATE_READ                 u8 = 0x26;
-static const CODE_EVALUATE_READ_RESPONSE        u8 = 0x27;
-static const CODE_EVALUATE_READ_MODULE          u8 = 0x28;
-static const CODE_EVALUATE_READ_MODULE_RESPONSE u8 = 0x29;
-static const CODE_LIST_RESOURCES_REQUEST        u8 = 0x2a;
-static const CODE_LIST_RESOURCES_RESPONSE       u8 = 0x2b;
-static const CODE_LIST_MODULES_REQUEST          u8 = 0x2c;
-static const CODE_LIST_MODULES_RESPONSE         u8 = 0x2d;
+const CODE_NEW_EVALUATOR                 :u8 = 0x20;
+const CODE_NEW_EVALUATOR_RESPONSE        :u8 = 0x21;
+const CODE_CLOSE_EVALUATOR               :u8 = 0x22;
+const CODE_EVALUATE                      :u8 = 0x23;
+const CODE_EVALUATE_RESPONSE             :u8 = 0x24;
+const CODE_EVALUATE_LOG                  :u8 = 0x25;
+const CODE_EVALUATE_READ                 :u8 = 0x26;
+const CODE_EVALUATE_READ_RESPONSE        :u8 = 0x27;
+const CODE_EVALUATE_READ_MODULE          :u8 = 0x28;
+const CODE_EVALUATE_READ_MODULE_RESPONSE :u8 = 0x29;
+const CODE_LIST_RESOURCES_REQUEST        :u8 = 0x2a;
+const CODE_LIST_RESOURCES_RESPONSE       :u8 = 0x2b;
+const CODE_LIST_MODULES_REQUEST          :u8 = 0x2c;
+const CODE_LIST_MODULES_RESPONSE         :u8 = 0x2d;
--- src/evaluator/msg_api/outgoing.rs
+++ src/evaluator/msg_api/outgoing.rs
@@ -1,8 +1,12 @@
-use rmp;
+use rmp_serde as rmps;
+
+use serde::{Serialize, Deserialize};
+use rmps::Serializer;
 
 /// Trait required to pass messages to the pkl server.
-trait OutgoingMessage {
-    fn to_msg_pack(&self) -> Result<&[u8], &'static str>;
+/// Note this must implement Serialize to work with rmps
+pub trait OutgoingMessage: Serialize {
+    fn to_msg_pack(&self) -> Result<Vec<u8>, &'static str>;
 }
 
 /// Packs a message in messagepasing v5 format
@@ -10,15 +14,47 @@ trait OutgoingMessage {
 /// # Example
 pub fn pack_message(msg: &impl OutgoingMessage, code: u8) -> Result<Vec<u8>, &'static str> {
     let mut buf = Vec::new();
-    match rmp::encode::write_u8(&mut buf, code) {
-        Err(_) => return Err("Failed to encode message passing code in message pack"),
-        _ => {},
-    };
-    match rmp::encode::write_bin(&mut buf, msg.to_msg_pack()
-                .expect("Failed to convert message passing msg to message pack binary representation")) {
-        Err(_) => return Err("Failed to encode mess"),
-        _ => {},
-    }
+    let value = (code, msg);
 
+    let _ = &value.serialize(&mut Serializer::new(&mut buf)).unwrap();
     return Ok(buf);
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ModuleReader {
+    scheme: String,
+    has_hierarchical_uris: bool,
+    is_globbable: bool,
+}
+
+impl OutgoingMessage for ModuleReader {
+    fn to_msg_pack(&self) -> Result<Vec<u8>, &'static str> {
+        let mut buf = Vec::new();
+
+        match &self.serialize(&mut Serializer::new(&mut buf).with_human_readable()) {
+            Ok(_) => return Ok(buf),
+            Err(_) => return Err("Failed to serialize"),
+        }
+
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_pack_message_module_reader() {
+        let mr = ModuleReader{scheme: "customfs".into(), has_hierarchical_uris: true, is_globbable: true};
+        let msp = pack_message(&mr, 0x20).unwrap();
+
+        let expected = vec![0x92, 0x1e, 0x83, 0xa6, 0x73, 0x63, 0x68, 0x65, 0x6d, 0x65, 0xa8, 0x63, 0x75, 0x73, 0x74, 0x6f,
+                            0x6d, 0x66, 0x73, 0xa9, 0x67, 0x6c, 0x6f, 0x62, 0x62, 0x61, 0x62, 0x6c, 0x65, 0xc3, 0xad, 0x68,
+                            0x61, 0x73, 0x4e, 0x65, 0x73, 0x74, 0x65, 0x64, 0x55, 0x72, 0x69, 0x6c, 0xc3,];
+
+        println!("Result of Serialization: {:X?}", msp);
+
+        let buf: (u8, ModuleReader) = rmp_serde::from_slice(&msp).unwrap();
+        println!("Result of Deserialization: {:?}", buf);
+    }
 }
