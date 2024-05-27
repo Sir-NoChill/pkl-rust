0b4844dc78edd13cc34f102c967f2f0176e942cd
Ayrton Chilibeck <aa@chilibeck.com>
2024-05-27 07:37:10 -0600

    Procedural Macro First Draft

--- test_suite/tests/pkl_derive/harness.rs
+++ test_suite/tests/pkl_derive/harness.rs
@@ -1,8 +1,8 @@
 #[test]
 fn test_construction() {
     let t = trybuild::TestCases::new();
-    t.pass("tests/pkl_derive/init.rs");
-    t.pass("tests/pkl_derive/simple.rs");
+    t.pass("tests/init.rs");
+    // t.pass("tests/macro/structure.rs");
     // t.pass("tests/macro/decompose.rs");
     // t.pass("tests/macro/construct.rs");
 }
