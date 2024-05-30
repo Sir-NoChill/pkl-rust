00a06d3e061d35419e2ff734c34cdfbf019dc283
Ayrton Chilibeck <aa@chilibeck.com>
2024-05-30 08:19:22 -0600

    Documentation

--- pkl-bind/src/lib.rs
+++ pkl-bind/src/lib.rs
@@ -15,9 +15,37 @@
 //! library, for which we thank the developers, to communicate with a
 //! spawned pkl server in a subprocess. Communication with the server
 //! occurs through message passing, however there are a few bugs to work out...
+//!
+//! ## Development Progress
+//!
+//! Currently the development process is ongoing, please see the
+//! [github repo](https://github.com/sir-nochill/pkl-rust) for a complete listing
+//! of all currently in progress features.
+//!
+//! ## Feature List
+//!
+//! We aim to conform to the pkl-lang specification for a language binding.
+//! The workflow will be as follows (once the crate is complete):
+//! - Write your pkl file
+//!   - AKA: your file that users should 'amend
+//! - Generate the binding module from your .pkl file
+//! - `use pkl` to get your configuration data
+//!
+//! Currently, the generation of the binding module is in the works,
+//! however carefully crafting both your .pkl file and your rust data structure
+//! will yeild the correct results. The target project structure is as follows:
+//! ```norun
+//! ProjectDir
+//! ├── PklDir
+//! └── src
+//!      ├── pkl
+//!      │    └── // generated sources
+//!      ├── main.rs
+//!      └── pkl.rs
+//! ```
+//! from which you can import the pkl structures
 
 ///////////////////////////////////////////////////////////////////////////
 
 #![cfg_attr(feature = "unstable", feature(*))]
-pub mod generator;
 pub mod evaluator;
